c75b346094a19f95383ffc94045df240e2cc3364
Erolca <erol.calisgan@gmail.com>
2018-04-06 21:22:13 +0300

    Generic

---
+++ 18.3.1. Generic Collection/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+class MainClass
+{
+    static void Main()
+    {
+        Collection<int> numbers = new Collection<int>();
+        numbers.Add(2);
+        numbers.Add(9);
+
+        Collection<string> strings = new Collection<string>();
+        strings.Add("J");
+        strings.Add("B");
+
+        Collection< Collection<int> > colNumbers = new Collection< Collection<int> >();
+        colNumbers.Add(numbers);
+
+        IList<int> theNumbers = numbers;
+        foreach (int i in theNumbers)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+//2
+//9
---
+++ 18.3.2. Extends Collection/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+class ConverterCollection<T> : Collection<T>
+{
+    private Converter<T, T> convert;
+
+    public ConverterCollection(Converter<T, T> convert)
+    {
+        this.convert = convert;
+    }
+
+    protected override void InsertItem(int index, T item)
+    {
+        base.InsertItem(index, convert(item));
+    }
+}
+
+public class MainClass
+{
+    public static void Main()
+    {
+        ConverterCollection<string> c = new ConverterCollection<string>(
+            delegate (string s) { return s.ToUpper(); });
+        c.Add("Hello");
+        c.Add("World!");
+
+        foreach (string s in c)
+            Console.WriteLine(s);
+    }
+}
+//HELLO
+//WORLD!
