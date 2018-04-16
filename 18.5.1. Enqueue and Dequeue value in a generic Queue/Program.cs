c76839ba886885cb9ff93b76b442388fe271ad05
erolcalisgan <erolcalisgan@hotmail.com>
2018-04-16 12:10:45 +0300

    Generic

---
+++ 18.5.1. Enqueue and Dequeue value in a generic Queue/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MainClass
+{
+    public static void Main()
+    {
+        Queue<double> q = new Queue<double>();
+
+        q.Enqueue(9.6);
+        q.Enqueue(2.0);
+        q.Enqueue(3.0);
+        q.Enqueue(3.1);
+
+        double sum = 0.0;
+        Console.Write("Queue contents: ");
+        while (q.Count > 0)
+        {
+            double val = q.Dequeue();
+            Console.Write(val + " ");
+            sum += val;
+        }
+
+        Console.WriteLine("\nTotal is " + sum);
+    }
+}
+//Queue contents: 9.6 2 3 3.1
+//Total is 17.7
---
+++ 18.6.1. Create and use a generic Dictionary/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+
+
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Dictionary<string, MyClass> MyClassDictionary = new Dictionary<string, MyClass>();
+
+        MyClassDictionary.Add("Crypto", new MyClass());
+
+        MyClass myClass = MyClassDictionary["Crypto"];
+
+        Console.WriteLine("Got from dictionary: {0}", myClass);
+
+
+
+    }
+}
+class MyClass
+{
+
+    public override string ToString()
+    {
+
+        return "my class";
+    }
+
+}
+//Got from dictionary: my class
